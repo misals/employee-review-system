66861f755ac0f5d0296477c6553cc1b650ceebc9
Suraj Misal <surajmisal18@gmail.com>
2022-12-01 16:27:25 +0530

    change

---
+++ index.js
@@ -0,0 +1,69 @@
+require('./config/database').connect();
+const express = require('express');
+const cookieParser = require('cookie-parser'); // parse cookie header and populate req.cookies
+const bodyParser = require('body-parser'); // parses incoming request bodies (req.body)
+const app = express();
+const { PORT, MONGODB_URL, SESSION_SECRET_KEY } = process.env;
+const expressLayouts = require('express-ejs-layouts');
+
+// used for session cookie
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
+const customMware = require('./config/middleware');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(cookieParser());
+
+app.use(expressLayouts);
+
+app.use(express.static('./assets'));
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+// set up view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// mongo store is used to store the session cookie in the db
+app.use(
+  session({
+    name: 'employee-review-system',
+    secret: SESSION_SECRET_KEY,
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 1000 * 60 * 100,
+    },
+    store: MongoStore.create({
+      mongoUrl: MONGODB_URL,
+      autoRemove: 'disabled',
+    }),
+    function(err) {
+      console.log(err || 'connect-mongodb setup ok');
+    },
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// sets the authenticated user in the response
+app.use(passport.setAuthenticatedUser);
+
+app.use(flash());
+app.use(customMware.setFlash);
+
+// use express router
+app.use('/', require('./routes'));
+
+app.listen(PORT || 5000, (err) => {
+  if (err) {
+    console.log(`Error in running the server: ${err}`);
+  }
+  console.log(`Server is running on port: ${PORT}`);
+});
